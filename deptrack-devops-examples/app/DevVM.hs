e6cf2d8b78d4abedb361bad0068cacebe44b541c
Arnaud Bailly <arnaud.oqube@gmail.com>
2017-05-10 06:20:38 +0200

    cleanup devbox VM example and add it to README

--- deptrack-devops-examples/app/DevVM.hs
+++ deptrack-devops-examples/app/DevVM.hs
@@ -3,37 +3,28 @@
 {-# LANGUAGE TupleSections     #-}
 module Main where
 
-import           Control.Monad             (forM, mapM, void)
-import           Data.Monoid
-import           Data.String.Conversions   (convertString)
+import           Control.Monad             (forM, void)
 import           Data.Text                 (pack)
-import           DepTrack
-import           Devops.Base               (DevOp, noop)
+import           Devops.Base               (DevOp)
 import           Devops.Bootstrap.Build
 import           Devops.Bootstrap.DO
 import           Devops.Bootstrap.Parasite
 import           Devops.Callback           (binaryCall, continueConst)
 import           Devops.Cli                (App (..), Method (..), SelfPath,
-                                            appMain, appMethod, methodArg,
-                                            simpleMain)
+                                            appMain, appMethod, methodArg)
 import           Devops.Debian.Base        (deb)
 import           Devops.Debian.Commands    (git)
 import           Devops.Debian.User        (Group (..), User (..),
                                             directoryPermissions, homeDirPath,
                                             mereUser, preExistingUser,
                                             userDirectory)
-import           Devops.Git                (GitRepo (..), gitClone, gitDir)
-import           Devops.Haskell            (stackPackage)
+import           Devops.Git                (GitRepo (..), gitClone)
 import           Devops.Optimize           (optimizeDebianPackages)
 import           Devops.OS
 import           Devops.Ref
-import           Devops.Storage            (DirectoryPresent (..), FileContent,
-                                            FileLinked, FilePresent (..),
-                                            directory, fileContent, fileLink,
-                                            preExistingFile, subdirectory,
-                                            (</>))
+import           Devops.Storage            (DirectoryPresent (..),
+                                            FilePresent (..), fileLink, (</>))
 import           Network.DO                hiding (error)
-import           System.Environment        (getArgs)
 import           System.IO                 (BufferMode (..), hSetBuffering,
                                             stderr, stdout)
 
@@ -47,7 +38,7 @@ data Stage = Local String Int
 parseStage :: [String] -> (Stage, Method)
 parseStage = \case
     ("_remote_":arg:[]) -> (Remote, appMethod arg)
-    (host:key:arg:[])   -> (Local host (read key),  appMethod arg)
+    (host:doKey:arg:[]) -> (Local host (read doKey),  appMethod arg)
     args                -> error $ "unparsed args: " ++ show args
 
 unparseStage :: Stage -> Method -> [String]
@@ -59,10 +50,10 @@ unparseStage stage m = case stage of
 
 main :: IO ()
 main = do
-  hSetBuffering stdout LineBuffering
-  hSetBuffering stderr LineBuffering
-  let app = App parseStage unparseStage stages [optimizeDebianPackages] :: App Stage
-  appMain app
+    hSetBuffering stdout LineBuffering
+    hSetBuffering stderr LineBuffering
+    let app = App parseStage unparseStage stages [optimizeDebianPackages] :: App Stage
+    appMain app
 
 stages :: Stage -> SelfPath -> (Stage -> Method -> [String]) -> DevOp ()
 stages Remote _ _         = devtools
@@ -71,12 +62,12 @@ stages (Local hn key)  self fixCall = void $ do
     let remoteCallback = binaryCall self (fixCall Remote)
 
     -- the droplet
-    let droplet = parasitedHost hn key
+    let doDroplet = parasitedHost hn key
 
     -- the remotely configured droplet
     remoteContinued root
-      (continueConst devtools remoteCallback)
-      droplet
+        (continueConst devtools remoteCallback)
+        doDroplet
 
 exe               = "deptrack-devops-example-devbox"
 root              = preExistingUser "root"
@@ -86,16 +77,16 @@ dropletConfig key = standardDroplet { size = G4, configImageSlug = ubuntuXenialS
 
 parasitedHost :: String -> Int -> DevOp ParasitedHost
 parasitedHost dropletName key = do
-  let host     = droplet False ((dropletConfig key) { configName = dropletName } )
-      built    = build ubuntu16_04 ".." exe
-      doref    = saveRef (pack dropletName)
-      resolved = resolveRef doref host
-  parasite root (buildOutput built) resolved
+    let host     = droplet False ((dropletConfig key) { configName = dropletName } )
+        built    = build ubuntu16_04 ".." exe
+        doref    = saveRef (pack dropletName)
+        resolved = resolveRef doref host
+    parasite root (buildOutput built) resolved
 
 devtools :: DevOp ()
 devtools = void $ do
-        packages
-        dotFiles devUser (dotFilesDir devUser)
+    packages
+    dotFiles devUser (dotFilesDir devUser)
 
 packages :: DevOp ()
 packages = void $ do
@@ -107,28 +98,27 @@ packages = void $ do
 
 dotFilesDir :: DevOp User -> DevOp DirectoryPresent
 dotFilesDir mkUsr = do
-  User u <- mkUsr
-  let home = (DirectoryPresent . homeDirPath . userName) <$> mkUsr
-      dotfilesDir = userDirectory "dotfiles" mkUsr
-      userAndGroup = (,Group u) <$> mkUsr
-  directoryPermissions userAndGroup dotfilesDir
+    User u <- mkUsr
+    let dotfilesDir = userDirectory "dotfiles" mkUsr
+        userAndGroup = (,Group u) <$> mkUsr
+    directoryPermissions userAndGroup dotfilesDir
 
 dotFiles :: DevOp User -> DevOp DirectoryPresent -> DevOp [FilePresent]
 dotFiles mkUsr dotFiles = do
-  let repo = gitClone "https://github.com/abailly/dotfiles" "master" git dotFiles
-  forM [ ".tmux.conf"
-       , ".emacs"
-       , ".gitconfig"
-       , ".bash_profile"
-       ] $ symlinkFile mkUsr repo
+    let repo = gitClone "https://github.com/abailly/dotfiles" "master" git dotFiles
+    forM [ ".tmux.conf"
+         , ".emacs"
+         , ".gitconfig"
+         , ".bash_profile"
+         ] $ symlinkFile mkUsr repo
 
 symlinkFile :: DevOp User -> DevOp GitRepo -> FilePath -> DevOp FilePresent
 symlinkFile mkUser mkRepo filepath = do
-  User u          <- mkUser
-  let home = homeDirPath u
-  fst <$> fileLink (home </> filepath) (preExistingFileIn mkRepo filepath)
+    User u          <- mkUser
+    let home = homeDirPath u
+    fst <$> fileLink (home </> filepath) (preExistingFileIn mkRepo filepath)
 
 preExistingFileIn :: DevOp GitRepo -> FilePath -> DevOp FilePresent
 preExistingFileIn mkRepo fp = do
-  GitRepo dir _ _ <- mkRepo
-  return $ FilePresent (getDirectoryPresentPath dir </> fp)
+    GitRepo dir _ _ <- mkRepo
+    return $ FilePresent (getDirectoryPresentPath dir </> fp)
