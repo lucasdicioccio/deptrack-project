ac3308dead5cdc413bf338d9883071f563d051d2
Lucas DiCioccio <lucas@dicioccio.fr>
2018-05-27 21:32:44 +0200

    Add Git clone w/o sudo.

--- deptrack-devops-recipes/src/Devops/Git.hs
+++ deptrack-devops-recipes/src/Devops/Git.hs
@@ -13,8 +13,9 @@ import           System.Posix.User       (getGroupEntryForID, getUserEntryForID,
 
 import           Devops.Base
 import           Devops.Binary
-import           Devops.Debian
+import           Devops.Debian (sudoRunAsInDir)
 import           Devops.Storage
+import           Devops.Utils (blindRun)
 
 type GitUrl = Text
 type GitBranch = Text
@@ -43,6 +44,31 @@ gitClone path branch mkGit mkDir = devop snd mkOp $ do
             username <- Text.pack . userName <$> getUserEntryForID (fileOwner status)
             groupname <- Text.pack . groupName <$> getGroupEntryForID (fileGroup status)
             let run = sudoRunAsInDir g d (username,groupname)
+            run ["clone", "-b", Text.unpack b, Text.unpack r, d] ""
+            run ["pull", "origin", Text.unpack b] ""
+            run ["submodule", "init"] ""
+            run ["submodule", "update"] ""
+
+gitCloneSimple
+  :: GitPath
+  -> GitBranch
+  -> DevOp env (Binary "git")
+  -> DevOp env DirectoryPresent
+  -> DevOp env GitRepo
+gitCloneSimple path branch mkGit mkDir = devop snd mkOp $ do
+  dir <- mkDir
+  g <- mkGit
+  return (g,(GitRepo dir path branch))
+  where mkOp (g,(GitRepo (DirectoryPresent d) r b)) = buildOp
+          ("git-repo: " <> convertString d)
+          ("clones " <> r <> " at " <> convertString d)
+          noCheck
+          (clone g d b r)
+          (noAction)
+          (noAction)
+
+        clone g d b r = do
+            let run = blindRun g
             run ["clone", "-b", Text.unpack b, Text.unpack r, d] ""
             run ["pull", "origin", Text.unpack b] ""
             run ["submodule", "init"] ""
